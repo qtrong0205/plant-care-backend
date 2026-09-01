c82cd475ad4b07ead130a0f8148145c6cb768e0b
qtrong0205 <quoctrong02052006@gmail.com>
2026-09-01 14:01:50 +0700

    Add User entity

---
+++ src/main/java/com/qtrong/plantcare/entity/User.java
@@ -0,0 +1,56 @@
+package com.qtrong.plantcare.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userId;
+    @Email
+    @UniqueElements
+    private String email;
+    private String password;
+    private String name;
+    private Date created_at;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+}
